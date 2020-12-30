14c58d49f9e62b870c893c2c269c930c8a7b6087
sudhanshusekhar56 <sudhanshusekhar56@hotmail.com>
2020-12-30 22:20:51 +0530

    minor changes

--- app.py
+++ app.py
@@ -1,32 +1,33 @@
 from flask import Flask, render_template, request
 import pickle
 
-app=Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/')
 def home():
-    return render_template("Main.html")
+    return render_template("home.html")
 
-data=[]
 
-@app.route('/pred' ,methods=['GET', 'POST'])
+data = []
+
+
+@app.route('/pred', methods=['POST', 'GET'])
 def predict():
-    if request == 'POST':
-        add=open('model.pkl', 'rb')
-        model=pickle.load(add)
+    if request.method == 'POST':
+        add = open('model.pkl', 'rb')
+        model = pickle.load(add)
 
-        ram=int(request.form['rm'])
-        pw=int(request.form['pw'])
-        ph=int(request.form['ph'])
-        bc=int(request.form['bc'])
-        ints=int(request.form['is'])
+        ram = int(request.form['rm'])
+        pw = int(request.form['pw'])
+        ph = int(request.form['ph'])
+        bc = int(request.form['bc'])
+        ints = int(request.form['is'])
 
-        data=[[ram,bc,pw,ph,ints]]
+        data = [[ram, bc, pw, ph, ints]]
         print(data)
-        res=model.predict(data)
-        
-        print(res)
-        return render_template('Main.html' ,result=res)
+        res = model.predict(data)
+        return render_template('home.html', result=res)
 
 
 if __name__ == "__main__":
